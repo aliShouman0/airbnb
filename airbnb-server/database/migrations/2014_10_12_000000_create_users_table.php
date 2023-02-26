a126c17f48f651b97bb796e3f801f1d19840ab7b
Ali Shouman <alishouman010@gmail.com>
2023-02-26 21:44:26 +0200

    Backend:Creating migration

--- airbnb-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ airbnb-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('DOB');
+            $table->string('phone');
+            $table->string('bio');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
