b13ce8c04eb14fc55182ae63dcfa164706317f39
Ali Alrida Shouman <alishouman010@gmail.com>
2023-03-02 11:56:39 +0200

    Adding test images for card

--- airbnb-user/src/pages/Home/Home.tsx
+++ airbnb-user/src/pages/Home/Home.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import Card from "../../components/Card/Card";
 import Navbar from "../../components/Navbar/Navbar";
+import image1 from "../../assets/1.jpeg";
+import image2 from "../../assets/2.jpeg";
+import image3 from "../../assets/3.jpeg";
+import image4 from "../../assets/4.jpg";
+import image5 from "../../assets/5.jpeg";
+
 import "./index.css";
 
 function Home() {
+  const images = [image1, image2, image3, image4, image5];
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <div className="container">
+        <Card images={images} />
+      </div>
     </>
   );
 }
